5c615783ebea5efd4ec6256ee04e4d02ba6c384e
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-11 01:13:27 +0900

    undo

--- wp-content/plugins/meta-by-url/meta-by-url.php
+++ wp-content/plugins/meta-by-url/meta-by-url.php
@@ -71,7 +71,7 @@ class Meta_By_Url {
 			'exclude_tree' => '',
 			'number' => '',
 			'offset' => 0,
-			'post_type' => 'post',//page|post
+			'post_type' => $post_type,//page|post
 			'post_status' => 'publish'
 		); 
 		$pages = get_pages( $args );  // get all pages based on supplied args
