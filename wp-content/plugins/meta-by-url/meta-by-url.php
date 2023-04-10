b8356db94bfc03a6d1f6732aa14c801c1874835f
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-10 18:58:01 +0900

    try to add menu

--- wp-content/plugins/meta-by-url/meta-by-url.php
+++ wp-content/plugins/meta-by-url/meta-by-url.php
@@ -14,3 +14,11 @@ Domain Path: /languages
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+
+
+function add_custom_menu_page()
+{
+    add_menu_page('Meta By URL', 'Meta By URL', 'manage_options', 'meta-by-url', 'meta-by-url-setting', 'dashicons-admin-generic', 4);
+}
+add_action('admin_menu', 'add_custom_menu_page');
