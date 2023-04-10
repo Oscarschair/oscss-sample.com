d43d265eb1bc7c18b7eefda92944ff62e9dae301
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-10 18:51:23 +0900

    a new plugin

---
+++ wp-content/plugins/meta-by-url/meta-by-url.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Meta By URL
+Description: URLごとにMetaデータを登録するプラグイン
+Version: 1.0
+Plugin URI: ---
+Author: Hirofumi Kuruma
+Author URI: https://oscarchair.jp/
+License: ---
+Text Domain: meta-by-url
+Domain Path: /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
