74a0cc09a43cb84e5d09b3dfe19c83f651d16a52
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-10 19:00:53 +0900

    60

--- wp-content/plugins/meta-by-url/meta-by-url.php
+++ wp-content/plugins/meta-by-url/meta-by-url.php
@@ -19,6 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function add_custom_menu_page()
 {
-    add_menu_page('Meta By URL', 'Meta By URL', 'manage_options', 'meta-by-url', 'meta-by-url-setting', 'dashicons-admin-generic', 40);
+    add_menu_page('Meta By URL', 'Meta By URL', 'manage_options', 'meta-by-url', 'meta-by-url-setting', 'dashicons-admin-generic', 60);
 }
 add_action('admin_menu', 'add_custom_menu_page');
