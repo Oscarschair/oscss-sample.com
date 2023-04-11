dd4e04931c53119f1f026e59766d312a0c0ad81e
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-11 19:53:05 +0900

    try

--- wp-content/plugins/meta-by-url/templates/portal.php
+++ wp-content/plugins/meta-by-url/templates/portal.php
@@ -1,72 +1,57 @@
 <h1 class="wp-heading-inline">Meta By URL</h1>
 
-<form method="post" action="options.php" enctype="multipart/form-data" encoding="multipart/form-data">
-    <?php
-   // settings_fields('custom-menu-group');
-   // do_settings_sections('custom-menu-group'); ?>
-    <div class="metabox-holder">
-      <div class="postbox ">
-        <h3 class='hndle'><span>テキスト</span></h3>
-        <div class="inside">
-          <div class="main">
-            <p class="setting_description">テキストを入力してください。</p>
-            <h4>テキスト</h4>
-            <p><input type="text" id="text" name="text" value="<?php //echo get_option('text'); ?>"></p>
-          </div>
-        </div>
-      </div>
-      <div class="postbox ">
-        <h3 class='hndle'><span>テキストボックス</span></h3>
-        <div class="inside">
-          <div class="main">
-            <p class="setting_description">テキストボックスを入力してください。</p>
-            <h4>テキストボックス</h4>
-            <textarea id="textbox" class="regular-text" name="textbox" rows="10" cols="60"><?php //echo get_option('textbox'); ?></textarea>
-          </div>
-        </div>
-      </div>
-      <div class="postbox ">
-        <h3 class='hndle'><span>セレクトボックス</span></h3>
-        <div class="inside">
-          <div class="main">
-            <p class="setting_description">セレクトボックスを選択してください。</p>
-            <h4>セレクトボックス</h4>
-            <select name="select" id="select">
-              <option value="0" <?php //selected(0, get_option('select')); ?> >選択してください</option>
-              <option value="1" <?php //selected(1, get_option('select')); ?> >セレクトボックス1</option>
-              <option value="2" <?php //selected(2, get_option('select')); ?> >セレクトボックス2</option>
-              <option value="3" <?php //selected(3, get_option('select')); ?> >セレクトボックス3</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="postbox ">
-        <h3 class='hndle'><span>ラジオボタン</span></h3>
-        <div class="inside">
-          <div class="main">
-            <p class="setting_description">ラジオボタンを選択してください。</p>
-            <h4>ラジオボタン</h4>
-            <ul>
-              <li><label><input name="radio" type="radio" value="0" <?php //checked(0, get_option('radio')); ?> />ラジオボタン0</label></li>
-              <li><label><input name="radio" type="radio" value="1" <?php //checked(1, get_option('radio')); ?> />ラジオボタン1</label></li>
-              <li><label><input name="radio" type="radio" value="2" <?php //checked(2, get_option('radio')); ?> />ラジオボタン2</label></li>
-              <li><label><input name="radio" type="radio" value="3" <?php //checked(3, get_option('radio')); ?> />ラジオボタン3</label></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="postbox ">
-        <h3 class='hndle'><span>チェックボックス</span></h3>
-        <div class="inside">
-          <div class="main">
-            <p class="setting_description">チェックボックスをチェックしてください。</p>
-            <h4>チェックボックス</h4>
-            <label><input name="checkbox" id="checkbox1" type="checkbox" value="1" <?php //checked(1, get_option('checkbox')); ?> />チェックボックス1</label>
-          </div>
-        </div>
-      </div>
-    </div>
-    <?php// submit_button(); ?>
-  </form>
+<table class="wp-list-table widefat fixed striped table-view-list posts">
+	<thead>
+	<tr>
+		<td id="cb" class="manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-1">すべて選択</label><input id="cb-select-all-1" type="checkbox"></td><th scope="col" id="title" class="manage-column column-title column-primary sortable desc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=title&amp;order=asc"><span>タイトル</span><span class="sorting-indicator"></span></a></th><th scope="col" id="author" class="manage-column column-author">投稿者</th><th scope="col" id="categories" class="manage-column column-categories">カテゴリー</th><th scope="col" id="tags" class="manage-column column-tags">タグ</th><th scope="col" id="comments" class="manage-column column-comments num sortable desc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=comment_count&amp;order=asc"><span><span class="vers comment-grey-bubble" title="コメント" aria-hidden="true"></span><span class="screen-reader-text">コメント</span></span><span class="sorting-indicator"></span></a></th><th scope="col" id="date" class="manage-column column-date sortable asc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=date&amp;order=desc"><span>日付</span><span class="sorting-indicator"></span></a></th>	</tr>
+	</thead>
+
+	<tbody id="the-list">
+				<tr id="post-104" class="iedit author-self level-0 post-104 type-post status-private format-standard hentry category-news">
+			<th scope="row" class="check-column">			<label class="screen-reader-text" for="cb-select-104">
+				６つ目のニュースを選択			</label>
+			<input id="cb-select-104" type="checkbox" name="post[]" value="104">
+			<div class="locked-indicator">
+				<span class="locked-indicator-icon" aria-hidden="true"></span>
+				<span class="screen-reader-text">
+				“６つ目のニュース” はロックされています				</span>
+			</div>
+			</th><td class="title column-title has-row-actions column-primary page-title" data-colname="タイトル"><div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
+<strong><a class="row-title" href="https://oscss-sample.com/wp-admin/post.php?post=104&amp;action=edit" aria-label="「６つ目のニュース」 (編集)">６つ目のニュース</a> — <span class="post-state">非公開</span></strong>
+
+<div class="hidden" id="inline_104">
+	<div class="post_title">６つ目のニュース</div><div class="post_name">６つ目のニュース</div>
+	<div class="post_author">1</div>
+	<div class="comment_status">closed</div>
+	<div class="ping_status">open</div>
+	<div class="_status">private</div>
+	<div class="jj">10</div>
+	<div class="mm">04</div>
+	<div class="aa">2023</div>
+	<div class="hh">11</div>
+	<div class="mn">38</div>
+	<div class="ss">41</div>
+	<div class="post_password"></div><div class="page_template">default</div><div class="post_category" id="category_104">1</div><div class="tags_input" id="post_tag_104"></div><div class="sticky"></div><div class="post_format"></div></div><div class="row-actions"><span class="edit"><a href="https://oscss-sample.com/wp-admin/post.php?post=104&amp;action=edit" aria-label="“６つ目のニュース” を編集">編集</a> | </span><span class="inline hide-if-no-js"><button type="button" class="button-link editinline" aria-label="「６つ目のニュース」をインラインでクイック編集" aria-expanded="false">クイック編集</button> | </span><span class="trash"><a href="https://oscss-sample.com/wp-admin/post.php?post=104&amp;action=trash&amp;_wpnonce=adf99ea409" class="submitdelete" aria-label="「６つ目のニュース」をゴミ箱に移動">ゴミ箱へ移動</a> | </span><span class="view"><a href="https://oscss-sample.com/2023/04/10/%ef%bc%96%e3%81%a4%e7%9b%ae%e3%81%ae%e3%83%8b%e3%83%a5%e3%83%bc%e3%82%b9/" rel="bookmark" aria-label="“６つ目のニュース” を表示">表示</a></span></div><button type="button" class="toggle-row"><span class="screen-reader-text">詳細を追加表示</span></button></td><td class="author column-author" data-colname="投稿者"><a href="edit.php?post_type=post&amp;author=1">管理者-オスカー</a></td><td class="categories column-categories" data-colname="カテゴリー"><a href="edit.php?category_name=news">ニュース</a></td><td class="tags column-tags" data-colname="タグ"><span aria-hidden="true">—</span><span class="screen-reader-text">タグなし</span></td><td class="comments column-comments" data-colname="コメント">		<div class="post-com-count-wrapper">
+		<span aria-hidden="true">—</span><span class="screen-reader-text">コメントはありません</span><span class="post-com-count post-com-count-pending post-com-count-no-pending"><span class="comment-count comment-count-no-pending" aria-hidden="true">0</span><span class="screen-reader-text">コメントはありません</span></span>		</div>
+		</td><td class="date column-date" data-colname="日付">最終更新日<br>2023年4月10日 11:38 AM</td>		</tr>
+				<tr id="post-102" class="iedit author-self level-0 post-102 type-post status-publish format-standard hentry category-news">
+			<th scope="row" class="check-column">			<label class="screen-reader-text" for="cb-select-102">
+				５つ目のニュースを選択			</label>
+			<input id="cb-select-102" type="checkbox" name="post[]" value="102">
+			<div class="locked-indicator">
+				<span class="locked-indicator-icon" aria-hidden="true"></span>
+				<span class="screen-reader-text">
+				“５つ目のニュース” はロックされています				</span>
+			</div>
+			</th>
+
+			</tbody>
+
+	<tfoot>
+	<tr>
+		<td class="manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-2">すべて選択</label><input id="cb-select-all-2" type="checkbox"></td><th scope="col" class="manage-column column-title column-primary sortable desc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=title&amp;order=asc"><span>タイトル</span><span class="sorting-indicator"></span></a></th><th scope="col" class="manage-column column-author">投稿者</th><th scope="col" class="manage-column column-categories">カテゴリー</th><th scope="col" class="manage-column column-tags">タグ</th><th scope="col" class="manage-column column-comments num sortable desc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=comment_count&amp;order=asc"><span><span class="vers comment-grey-bubble" title="コメント" aria-hidden="true"></span><span class="screen-reader-text">コメント</span></span><span class="sorting-indicator"></span></a></th><th scope="col" class="manage-column column-date sortable asc"><a href="https://oscss-sample.com/wp-admin/edit.php?orderby=date&amp;order=desc"><span>日付</span><span class="sorting-indicator"></span></a></th>	</tr>
+	</tfoot>
+
+</table>
 
 <?php $this->portal_get_pages();?>
