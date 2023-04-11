932dab01a78e8540ee526ae80520c6ecfc9a9aa6
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-04-11 19:22:40 +0900

    try get post

--- wp-content/plugins/meta-by-url/meta-by-url.php
+++ wp-content/plugins/meta-by-url/meta-by-url.php
@@ -54,34 +54,50 @@ class Meta_By_Url {
         return $template_path;
     }
 
-	function portal_get_pages($post_type = 'page') {// $pages, $pagenum = 1, $per_page = 20
+	function portal_get_pages($post_type = 'page') {
+		$post_data = get_post();
+		$ID        = $post_data->ID;                // ページID
+		$title     = $post_data->post_title;        // タイトル
+		$date      = $post_data->post_date;         // 投稿日時
+		$content   = $post_data->post_content;      // コンテンツ内容
+		$slug      = $post_data->post_name;         // スラッグ
+		$parent    = $post_data->post_parent;       // 親のID
+		$url       = $post_data->guid;              // ページURL
 
-		echo "post_type: ".$post_type."<br>";
-		$args = array(
-			'sort_order' => 'ASC',
-			'sort_column' => 'post_title',
-			'hierarchical' => 1,
-			'exclude' => '',
-			'include' => '',
-			'meta_key' => '',
-			'meta_value' => '',
-			'authors' => '',
-			'child_of' => 0,
-			'parent' => -1,
-			'exclude_tree' => '',
-			'number' => '',
-			'offset' => 0,
-			'post_type' => $post_type,//page|post
-			'post_status' => 'publish'
-		); 
-		$pages = get_pages( $args );  // get all pages based on supplied args
-		
-		foreach($pages as $page){ // $pages is array of object
-			echo "ID:".$page->ID."URL:".$page->guid."post_title:".$page->post_title."post_type:".$page->post_type."<br>";
-		//    $page_template = get_post_meta($page->ID, '_wp_page_template', true); // Page template stored in "_wp_page_template"
-		//    echo $page_template;
-		}
+		echo($ID);
+
 	}
+
+
+
+	// function portal_get_pages($post_type = 'page') {// $pages, $pagenum = 1, $per_page = 20
+
+	// 	echo "post_type: ".$post_type."<br>";
+	// 	$args = array(
+	// 		'sort_order' => 'ASC',
+	// 		'sort_column' => 'post_title',
+	// 		'hierarchical' => 1,
+	// 		'exclude' => '',
+	// 		'include' => '',
+	// 		'meta_key' => '',
+	// 		'meta_value' => '',
+	// 		'authors' => '',
+	// 		'child_of' => 0,
+	// 		'parent' => -1,
+	// 		'exclude_tree' => '',
+	// 		'number' => '',
+	// 		'offset' => 0,
+	// 		'post_type' => $post_type,//page|post
+	// 		'post_status' => 'publish'
+	// 	); 
+	// 	$pages = get_pages( $args );  // get all pages based on supplied args
+		
+	// 	foreach($pages as $page){ // $pages is array of object
+	// 		echo "ID:".$page->ID."URL:".$page->guid."post_title:".$page->post_title."post_type:".$page->post_type."<br>";
+	// 	//    $page_template = get_post_meta($page->ID, '_wp_page_template', true); // Page template stored in "_wp_page_template"
+	// 	//    echo $page_template;
+	// 	}
+	// }
 
 }
 
